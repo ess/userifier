7c674c3170593125790e198ccfeb0ce9cd0f9f27
Dennis Walters <dwalters@engineyard.com>
2015-09-25 19:56:36 -0400

    broke out Config

---
+++ config/config.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+  "os"
+  "fmt"
+  "github.com/BurntSushi/toml"
+)
+
+type Config struct {
+  File string
+  ApiId  string `toml:"api_id"`
+  ApiKey string `toml:"api_key"`
+}
+
+func New(config_file string) (config *Config) {
+  config = &Config{File: config_file}
+  if _, err := toml.DecodeFile(config_file, &config); err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+  }
+
+  return config
+}
--- main.go
+++ main.go
@@ -3,37 +3,17 @@ package main
 import (
   "fmt"
   "flag"
-  "github.com/BurntSushi/toml"
-  "os"
   "github.com/ess/userifier/user"
   "github.com/ess/userifier/client"
+  "github.com/ess/userifier/config"
 )
-
-type userifierConfig struct {
-  ApiId  string `toml:"api_id"`
-  ApiKey string `toml:"api_key"`
-}
-
-func read_config(config_file string) userifierConfig {
-  var config userifierConfig
-
-  if _, err := toml.DecodeFile(config_file, &config); err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
-
-  return config
-}
 
 func main() {
   var config_file = flag.String("config", "/opt/userify/config.toml", "config file to use")
   flag.Parse()
 
-  config := read_config(*config_file)
-
-
+  config := config.New(*config_file)
   client := client.New(config.ApiId, config.ApiKey)
-
   users := user.GetUsers(client.UserJSON())
 
   for _, user := range users {
