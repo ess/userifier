483c573ac62b5455a2dd90170c9243151916888e
Dennis Walters <dwalters@engineyard.com>
2015-09-28 02:46:21 -0400

    gofmt

--- config/config.go
+++ config/config.go
@@ -1,26 +1,26 @@
 package config
 
 import (
-  "os"
-  "fmt"
-  "github.com/BurntSushi/toml"
+	"fmt"
+	"github.com/BurntSushi/toml"
+	"os"
 )
 
 type Config struct {
-  File string
-  ApiId  string `toml:"api_id"`
-  ApiKey string `toml:"api_key"`
+	File   string
+	ApiId  string `toml:"api_id"`
+	ApiKey string `toml:"api_key"`
 }
 
 func New(config_file string) (config *Config) {
-  config = &Config{File: config_file}
-  config.readConfigFile()
-  return config
+	config = &Config{File: config_file}
+	config.readConfigFile()
+	return config
 }
 
 func (config *Config) readConfigFile() {
-  if _, err := toml.DecodeFile(config.File, &config); err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
+	if _, err := toml.DecodeFile(config.File, &config); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
--- main.go
+++ main.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-  "fmt"
-  "flag"
-  "github.com/ess/userifier/user"
-  "github.com/ess/userifier/client"
-  "github.com/ess/userifier/config"
+	"flag"
+	"fmt"
+	"github.com/ess/userifier/client"
+	"github.com/ess/userifier/config"
+	"github.com/ess/userifier/user"
 )
 
 func main() {
-  var config_file = flag.String(
-    "config",
-    "/opt/userify/config.toml",
-    "config file to use")
-  flag.Parse()
+	var config_file = flag.String(
+		"config",
+		"/opt/userify/config.toml",
+		"config file to use")
+	flag.Parse()
 
-  config := config.New(*config_file)
-  client := client.New(config.ApiId, config.ApiKey)
-  users := user.GetUsers(client.UserJSON())
+	config := config.New(*config_file)
+	client := client.New(config.ApiId, config.ApiKey)
+	users := user.UsersFromJSON(client.UserJSON())
 
-  for _, user := range users {
-    fmt.Println(user.String())
-  }
+	for _, user := range users {
+		fmt.Println(user.String())
+	}
 }
